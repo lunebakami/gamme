95d39d5bfecf15c793f7f9cd6c013ecba8265e37
Luis Santos <luis.santos.taiba@gmail.com>
2025-11-10 14:12:50 -0300

    fix: remove elysia code

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,1 @@
-import { Elysia } from 'elysia';
-import { websocket } from '@elysiajs/websocket';
-import { GAME_CONFIG } from '@gamme/shared';
-
-const app = new Elysia()
-  .use(websocket())
-  .get('/', () => 'Game Server Running')
-  .listen(3000);
-
-console.log(`🎮 Server running at ${app.server?.hostname}:${app.server?.port}`);
-console.log(`Game config:`, GAME_CONFIG);
+console.log(`Game config:`);
